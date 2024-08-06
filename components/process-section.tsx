9cd2a5b04e870fe1d0ca6f62297f56735b6198fb
0xSimbaDev <0xSimbaDev>
2024-08-06 12:39:00 +0000

    Pushed from Locofy Builder

--- components/process-section.tsx
+++ components/process-section.tsx
@@ -7,7 +7,7 @@ export type ProcessSectionType = {
 const ProcessSection: NextPage<ProcessSectionType> = ({ className = "" }) => {
   return (
     <div
-      className={`self-stretch h-[895px] flex flex-col items-center justify-center gap-[70px] bg-[url('/process-section@3x.png')] bg-cover bg-no-repeat bg-[top] text-center text-17xl text-white font-montserrat ${className}`}
+      className={`self-stretch h-[895px] flex flex-col items-center justify-center py-0 px-[150px] box-border gap-[70px] bg-[url('/process-section@3x.png')] bg-cover bg-no-repeat bg-[top] text-center text-17xl text-white font-montserrat ${className}`}
     >
       <b className="w-[318px] relative inline-block">Our Process</b>
       <div className="flex flex-row items-center justify-center gap-[30px]">
@@ -17,7 +17,7 @@ const ProcessSection: NextPage<ProcessSectionType> = ({ className = "" }) => {
           src="/content-image1@2x.png"
         />
         <img
-          className="w-[772px] relative h-[672px] object-cover"
+          className="w-[619px] relative h-[539px] object-cover"
           alt=""
           src="/processes-image@2x.png"
         />
