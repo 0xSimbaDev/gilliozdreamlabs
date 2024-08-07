9921ea621dbaa8a320db56d638237c697613f42f
0xSimbaDev <0xSimbaDev>
2024-08-07 04:22:53 +0000

    Pushed from Locofy Builder

--- components/why-choose-us.tsx
+++ components/why-choose-us.tsx
@@ -25,100 +25,24 @@ const WhyChooseUs: NextPage<WhyChooseUsType> = ({ className = "" }) => {
           />
         </div>
         <div className="flex flex-row items-start justify-start gap-[30px] md:flex-col">
-          <div className="w-[380px] flex flex-row items-start justify-start">
-            <div className="w-[130px] relative h-[140px]">
-              <div className="absolute top-[0px] left-[0px] shadow-[0px_7px_16px_rgba(0,_0,_0,_0.25)] rounded-2xl bg-gray-1000 w-[70px] h-[70px] overflow-hidden flex flex-row items-center justify-center p-[15px] box-border">
-                <img
-                  className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full"
-                  alt=""
-                  src="/riteamfill.svg"
-                />
-              </div>
-              <img
-                className="absolute top-[0px] left-[100px] max-w-full overflow-hidden h-[140px]"
-                alt=""
-                src="/vector-11.svg"
-              />
-            </div>
-            <div className="self-stretch flex-1 flex flex-col items-start justify-start gap-[15px]">
-              <b className="self-stretch relative">Expert Team</b>
-              <div className="self-stretch flex-1 relative text-xs">
-                Seasoned software developers and strategists with diverse
-                technical expertise.
-              </div>
-            </div>
-          </div>
-          <div className="w-[380px] h-[140px] flex flex-row items-start justify-start">
-            <div className="w-[130px] relative h-[140px]">
-              <div className="absolute top-[0px] left-[0px] shadow-[0px_7px_16px_rgba(0,_0,_0,_0.25)] rounded-2xl bg-gray-1000 w-[70px] h-[70px] overflow-hidden flex flex-row items-center justify-center p-[15px] box-border">
-                <img
-                  className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full"
-                  alt=""
-                  src="/hugeiconsworkflowcircle05.svg"
-                />
-              </div>
-              <img
-                className="absolute top-[0px] left-[100px] max-w-full overflow-hidden h-[140px]"
-                alt=""
-                src="/vector-11.svg"
-              />
-            </div>
-            <div className="self-stretch flex-1 flex flex-col items-start justify-start gap-[22px]">
-              <b className="self-stretch relative">Workflow Optimization</b>
-              <div className="self-stretch flex-1 relative text-xs">
-                We leverage our experience to optimize your workflows and
-                enhance customer experiences.
-              </div>
-            </div>
-          </div>
+          <WCUComponent
+            title="Expert Team"
+            description="Seasoned software developers and strategists with diverse technical expertise."
+          />
+          <WCUComponent
+            title="Workflow Optimization"
+            description="We leverage our experience to optimize your workflows and enhance customer experiences."
+          />
         </div>
         <div className="flex flex-row items-start justify-start gap-[30px] md:flex-col">
-          <div className="w-[380px] flex flex-row items-start justify-start">
-            <div className="w-[130px] relative h-[140px]">
-              <div className="absolute top-[0px] left-[0px] shadow-[0px_7px_16px_rgba(0,_0,_0,_0.25)] rounded-2xl bg-gray-1000 w-[70px] h-[70px] overflow-hidden flex flex-row items-center justify-center p-[15px] box-border">
-                <img
-                  className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full"
-                  alt=""
-                  src="/antdesignsolutionoutlined.svg"
-                />
-              </div>
-              <img
-                className="absolute top-[0px] left-[100px] max-w-full overflow-hidden h-[140px]"
-                alt=""
-                src="/vector-11.svg"
-              />
-            </div>
-            <div className="self-stretch flex-1 flex flex-col items-start justify-start gap-[15px]">
-              <b className="self-stretch relative">Custom Notion Solutions</b>
-              <div className="self-stretch flex-1 relative text-xs">
-                We create tailored Notion workspaces that perfectly align with
-                your unique business needs.
-              </div>
-            </div>
-          </div>
-          <div className="w-[380px] h-[140px] flex flex-row items-start justify-start">
-            <div className="w-[130px] relative h-[140px]">
-              <div className="absolute top-[0px] left-[0px] shadow-[0px_7px_16px_rgba(0,_0,_0,_0.25)] rounded-2xl bg-gray-1000 w-[70px] h-[70px] overflow-hidden flex flex-row items-center justify-center p-[15px] box-border">
-                <img
-                  className="self-stretch flex-1 relative max-w-full overflow-hidden max-h-full"
-                  alt=""
-                  src="/carbonintegration.svg"
-                />
-              </div>
-              <img
-                className="absolute top-[0px] left-[100px] max-w-full overflow-hidden h-[140px]"
-                alt=""
-                src="/vector-11.svg"
-              />
-            </div>
-            <div className="self-stretch flex-1 flex flex-col items-start justify-start gap-[15px]">
-              <b className="self-stretch relative">Seamless Integration</b>
-              <div className="self-stretch flex-1 relative text-xs">
-                Our proficiency in various development tools ensures a smooth
-                and tailored Notion experience.
-              </div>
-            </div>
-          </div>
+          <WCUComponent
+            title="Custom Notion Solutions"
+            description="We create tailored Notion workspaces that perfectly align with your unique business needs."
+          />
+          <WCUComponent
+            title="Seamless Integration"
+            description="Our proficiency in various development tools ensures a smooth and tailored Notion experience."
+          />
         </div>
       </div>
     </div>
