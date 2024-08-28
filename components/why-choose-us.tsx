2844ea2c890a35d834f03398178eae8efff82298
0xSimbaDev <brandonneil0309@gmail.com>
2024-08-28 21:50:05 +0800

    Refactored WhyChooseUs: removed redundant classes, adjusted content for smaller screens

--- components/why-choose-us.tsx
+++ components/why-choose-us.tsx
@@ -8,17 +8,17 @@ export type WhyChooseUsType = {
 const WhyChooseUs: NextPage<WhyChooseUsType> = ({ className = "" }) => {
   return (
     <div
-      className={`self-stretch flex flex-col items-center justify-center py-[285px] px-[150px] box-border gap-[70px] bg-[url('/home-section@3x.png')] bg-cover bg-no-repeat bg-[center] text-center text-17xl text-white font-montserrat ${className}`}
+      className={`flex flex-col items-center justify-center py-[285px] px-[150px] gap-[70px] bg-[url('/home-section@3x.png')] bg-cover bg-no-repeat bg-[center] text-center text-white font-montserrat md:px-[30px] md:py-[70px] ${className}`}
     >
-      <div className="self-stretch flex flex-col items-center justify-center gap-[30px]">
-        <h1 className="m-0 relative text-inherit font-bold font-[inherit]">
-        Expertise That Sets Us Apart
+      <div className="flex flex-col items-center justify-center gap-[30px] sm:max-w-sm">
+        <h1 className="text-[1.5rem] m-0 font-bold sm:text-[1.2rem]"> {/* Removed text-inherit and font-[inherit] */}
+          Expertise That Sets Us Apart
         </h1>
-        <div className="relative text-lg text-center">
-        Transforming Workflows, Maximizing Productivity
+        <div className="text-[1rem] text-center sm:text-[0.8rem]">
+          Transforming Workflows, Maximizing Productivity
         </div>
       </div>
-      <div className="self-stretch flex flex-col items-center justify-center gap-[30px] text-left text-lg md:flex-col md:items-center md:justify-center">
+      <div className="flex flex-col items-center justify-center gap-[30px] text-left md:flex-col md:items-center md:justify-center">
         <div className="flex flex-row items-start justify-start gap-[30px] md:flex-col">
           <WCUComponent
             title="Partners in Growth"
