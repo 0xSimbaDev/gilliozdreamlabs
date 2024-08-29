58cde815f9c67a0caf028575c1319311cb4fa2b6
0xSimbaDev <brandonneil0309@gmail.com>
2024-08-29 17:16:38 +0800

    Added translations and refactored HomeSection component

--- components/home-section.tsx
+++ components/home-section.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
-
+import { useLanguage } from "../context/LanguageContext";
 export type HomeSectionType = {
   className?: string;
 };
+
+type Language = "ENG" | "FR"; 
 
 const HomeSection: NextPage<HomeSectionType> = ({ className = "" }) => {
   const handleContactClick = () => {
@@ -11,25 +13,41 @@ const HomeSection: NextPage<HomeSectionType> = ({ className = "" }) => {
       contactSection.scrollIntoView({ behavior: 'smooth' });
     }
   };
+
+  const { language } = useLanguage() as { language: Language};
+
+  const translations = {
+    ENG: {
+      headline: "Simplify, Streamline, Succeed: Quantboost's Notion Solutions",
+      subheading: "Empower Your Team, Eliminate Chaos, Achieve More",
+      buttonText: "Contact Us"
+    },
+    FR: {
+      headline: "Simplifier, Rationaliser, Réussir: Les Solutions Notion de Quantboost",
+      subheading: "Renforcez votre équipe, éliminez le chaos, accomplissez davantage",
+      buttonText: "Contactez-nous"
+    }
+  };
+
   return (
-        <section
-          className={`flex flex-col items-center justify-center bg-[url('/home-section@3x.png')] bg-cover bg-no-repeat bg-[center] text-white font-montserrat h-screen p-6 md:p-10 ${className}`}
+    <section
+      className={`flex flex-col items-center justify-center bg-[url('/home-section@3x.png')] bg-cover bg-no-repeat bg-[center] text-white font-montserrat h-screen p-6 md:p-10 ${className}`}
+    >
+      <div className="text-content w-full text-center">
+        <h1 className="text-[1.5rem] font-bold mb-4 sm:text-[1.2rem]">
+          {translations[language].headline}
+        </h1>
+        <h3 className="text-[1rem] font-light mb-6 sm:text-[0.8rem]">
+          {translations[language].subheading}
+        </h3>
+        <button 
+          className="bg-blueviolet text-white text-bold font-montserrat py-3 px-6 rounded-full shadow-lg hover:bg-opacity-90 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blueviolet"
+          onClick={handleContactClick} 
         >
-          <div className="text-content w-full text-center">
-            <h1 className="text-[1.5rem] font-bold mb-4 sm:text-[1.2rem]">
-              Simplify, Streamline, Succeed: Quantboost's Notion Solutions
-           </h1>
-           <h3 className="text-[1rem] font-light mb-6 sm:text-[0.8rem]">
-              Empower Your Team, Eliminate Chaos, Achieve More
-           </h3>
-            <button 
-              className="bg-blueviolet text-white text-bold font-montserrat py-3 px-6 rounded-full shadow-lg hover:bg-opacity-90 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blueviolet"
-              onClick={handleContactClick} 
-            >
-              Contact Us
-            </button>
-          </div>
-        </section>
+          {translations[language].buttonText}
+        </button>
+      </div>
+    </section>
   );
 };
 
