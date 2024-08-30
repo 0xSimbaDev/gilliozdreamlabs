2b93798ee3035dfba97ecbe2a49c1914ac76feac
0xSimbaDev <brandonneil0309@gmail.com>
2024-08-30 23:24:33 +0800

    Create root types folder and index.ts for centralized type definitions

---
+++ types/index.ts
@@ -0,0 +1,52 @@
+// components/contact-section.tsx
+export type ContactSectionType = {
+  className?: string;
+};
+
+// components/home-section.tsx
+export type HomeSectionType = {
+  className?: string;
+};
+  
+// components/header.tsx
+export type HeaderType = {
+  className?: string;
+};
+
+// components/problem-section.tsx
+export type ProblemSectionType = {
+  className?: string;
+};
+
+// components/process-section.tsx
+export type ProcessSectionType = {
+  className?: string;
+};
+
+// components/solution-section.tsx
+export type SolutionSection1Type = {
+  className?: string;
+};
+
+// components/SolutionComponent.tsx
+export type SolutionComponentType = {
+  className?: string;
+  heading?: string;
+  subHeading?: string;
+};
+
+// components/WCUComponent.tsx 
+export type WCUComponentType = {
+  className?: string;
+  title?: string;
+  description?: string;
+  icon?: string;
+};
+
+// components/why-choose-us.tsx 
+export type WhyChooseUsType = {
+  className?: string;
+};
+
+// context/LanguageContext.tsx
+export type Language = "ENG" | "FR";
