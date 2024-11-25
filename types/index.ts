624463399f607b2cbcad78913e66cfcad1346e61
0xSimbaDev <brandonneil0309@gmail.com>
2024-11-25 20:59:49 +0800

    Added Types

--- types/index.ts
+++ types/index.ts
@@ -50,3 +50,35 @@ export type WhyChooseUsType = {
 
 // context/LanguageContext.tsx
 export type Language = "ENG" | "FR";
+
+// components/home/hero/hero-section.tsx
+export type HeroSectionType = {
+  className?: string;
+};
+
+// components/home/services/hero-section.tsx
+export type ServicesSectionType = {
+  className?: string;
+}
+
+// components/home/about/about-section.tsx
+export type AboutComponentType = {
+  className?: string;
+};
+
+// components/home/services/ServicesComponent.tsx
+export type ServicesComponentType = {
+  className?: string;
+  title?: string;
+  description?: string;
+  icon?: string;
+};
+
+// components/home/about/DropdownItem.tsx
+export type DropdownItemProps = {
+  id: number;
+  title: string;
+  content: string;
+  isOpen: boolean;
+  toggleDropdown: (id: number) => void;
+};
