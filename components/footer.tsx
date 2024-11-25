6fdadf0db1a73c9c53c60cdade3229b1f74cf92c
0xSimbaDev <brandonneil0309@gmail.com>
2024-11-25 20:54:45 +0800

    Added Footer

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='h-[100px] w-full flex justify-center items-center sm:text-xs md:text-base lg:text-xs font-montserrat'>
+            (c) 2024. GilliozDreamLabs. All Rights Reserved.
+        </div>
+    )
+}
+
+export default Footer
