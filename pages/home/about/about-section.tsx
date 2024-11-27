f4b83be3f54c8638ae35432d4611a544e5ae3336
0xSimba <110355857+0xSimbaDev@users.noreply.github.com>
2024-11-28 02:08:27 +0800

    Animate Section with Framer Motion (#11)

--- pages/home/about/about-section.tsx
+++ pages/home/about/about-section.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import { AboutComponentType } from '../../../types';
 import DropdownItem from './DropdownItem';
+import { motion } from "motion/react"
+
 
 const AboutSection: NextPage<AboutComponentType> = () => {
     const [openDropdown, setOpenDropdown] = useState<number | null>(null);
@@ -14,61 +16,73 @@ const AboutSection: NextPage<AboutComponentType> = () => {
         <section className="h-full flex items-center justify-center p-6 mt-[5rem]">
             <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-8">
                 {/* Left Image Section */}
-                <div className="flex justify-center items-center">
-                <Image
-                    src="/software_development_illustration_.png"
-                    alt="Software Development Illustration"
-                    width={400}
-                    height={400}
-                    className="rounded-md object-cover"
-                    priority
-                />
-                </div>
+                <motion.div 
+                    initial="hidden"
+                    whileInView={{ opacity: 1, x:10 }}
+                    transition={{duration: 1}}
+                    viewport={{ once: true, amount: 0.5 }}
+                    className="flex justify-center items-center"
+                >
+                    <Image
+                        src="/software_development_illustration_.png"
+                        alt="Software Development Illustration"
+                        width={400}
+                        height={400}
+                        className="rounded-md object-cover"
+                        priority
+                    />
+                </motion.div>
 
                 {/* Right Text Section */}
-                <div className="flex flex-col items-start font-montserrat text-dark_bg">
-                {/* Subheading */}
-                <h3 className="text-sm font-semibold uppercase tracking-wide sm:text-base md:text-lg lg:text-xl">
-                    About Us
-                </h3>
+                <motion.div 
+                    initial="hidden"
+                    whileInView={{ opacity: 1, x:-10 }}
+                    transition={{duration: 1}}
+                    viewport={{ once: true, amount: 0.5 }}
+                    className="flex flex-col items-start font-montserrat text-dark_bg"
+                >
+                    {/* Subheading */}
+                    <h3 className="text-sm font-semibold uppercase tracking-wide sm:text-base md:text-lg lg:text-xl">
+                        About Us
+                    </h3>
 
-                {/* Title */}
-                <h1 className="text-4xl font-bold mt-2 sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-blueviolet">
-                    Building a Smarter Digital Future Together
-                </h1>
+                    {/* Title */}
+                    <h1 className="text-4xl font-bold mt-2 sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl text-blueviolet">
+                        Building a Smarter Digital Future Together
+                    </h1>
 
-                {/* Body Text */}
-                <p className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">
-                    We specialize in delivering innovative technology solutions tailored to
-                    drive growth and success. With a focus on excellence and collaboration, we
-                    empower businesses to achieve their goals in a rapidly evolving digital landscape.
-                </p>
+                    {/* Body Text */}
+                    <p className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+                        We specialize in delivering innovative technology solutions tailored to
+                        drive growth and success. With a focus on excellence and collaboration, we
+                        empower businesses to achieve their goals in a rapidly evolving digital landscape.
+                    </p>
 
-                {/* Dropdown Section */}
-                <div className="max-w-3xl w-full">
-                    <DropdownItem
-                    id={1}
-                    title="Our Mission and Vision"
-                    content="Committed to redefining innovation and delivering solutions that matter."
-                    isOpen={openDropdown === 1}
-                    toggleDropdown={toggleDropdown}
-                    />
-                    <DropdownItem
-                    id={2}
-                    title="Our Unique Advantages"
-                    content="Leveraging cutting-edge technologies to provide superior value."
-                    isOpen={openDropdown === 2}
-                    toggleDropdown={toggleDropdown}
-                    />
-                    <DropdownItem
-                    id={3}
-                    title="Our Expert Team"
-                    content="A group of seasoned professionals driving impactful results."
-                    isOpen={openDropdown === 3}
-                    toggleDropdown={toggleDropdown}
-                    />
-                </div>
-                </div>
+                    {/* Dropdown Section */}
+                    <div className="max-w-3xl w-full">
+                        <DropdownItem
+                        id={1}
+                        title="Our Mission and Vision"
+                        content="Committed to redefining innovation and delivering solutions that matter."
+                        isOpen={openDropdown === 1}
+                        toggleDropdown={toggleDropdown}
+                        />
+                        <DropdownItem
+                        id={2}
+                        title="Our Unique Advantages"
+                        content="Leveraging cutting-edge technologies to provide superior value."
+                        isOpen={openDropdown === 2}
+                        toggleDropdown={toggleDropdown}
+                        />
+                        <DropdownItem
+                        id={3}
+                        title="Our Expert Team"
+                        content="A group of seasoned professionals driving impactful results."
+                        isOpen={openDropdown === 3}
+                        toggleDropdown={toggleDropdown}
+                        />
+                    </div>
+                </motion.div>
             </div>
         </section>
     );
